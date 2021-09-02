5925245b59a64b1ea258b227d1ba57aa429e08be
Gustavo Ornaghi Antunes <gustavoornaghiantunes@gmail.com>
2021-09-02 14:13:15 -0300

    conta por teclado adicionado e verificações feitas

--- main.js
+++ main.js
@@ -7,7 +7,19 @@ function verificarValor(){
     }
 }
 
-verificarValor()
+function apagar(){
+    let valor = document.querySelector("#conta").value;
+    let res = valor.slice(0, -1);
+    document.querySelector("#conta").value = res;
+    if(res === ""){
+        document.querySelector(".apagar").style.display = 'none';
+    }
+}
+
+function ultimoCaracter(){
+    return document.querySelector("#conta").value.charAt(document.querySelector("#conta").value.length - 1);
+}
+verificarValor();
 
 document.querySelectorAll(".botao").forEach(item => {
     item.addEventListener("click", function() {
@@ -36,6 +48,9 @@ document.querySelectorAll(".botao").forEach(item => {
                         alert("calculadora desligada!");
                     }
                 break;
+                case '=':
+                    alert('deu enter!');
+                break;
                 default:
                     if(document.querySelector("#conta").value === "LIGADO"){
                         if(this.className.indexOf("operador") != -1){
@@ -45,34 +60,88 @@ document.querySelectorAll(".botao").forEach(item => {
                         }
                     }else if(document.querySelector("#conta").value === "DESLIGADO"){
                         alert("calculadora desligada!");
-                    }else if(document.querySelector("#conta").value === ""){
-                        if(this.className.indexOf("operador") != -1){
-                        }else{
-                            document.querySelector("#conta").value += this.innerHTML.trim();
-                        }
                     }else{
-                        document.querySelector("#conta").value += this.innerHTML.trim();
+                        if(ultimoCaracter() >= 0 && ultimoCaracter() <=9){
+                            if(this.innerHTML.trim() >= 0 && this.innerHTML.trim() <=9){
+                                document.querySelector("#conta").value += this.innerHTML.trim();
+                            }else{
+                                if(ultimoCaracter() != ""){
+                                    document.querySelector("#conta").value += this.innerHTML.trim();
+                                }
+                            }
+                        }
                     }
                 break;
             }
         }
-        verificarValor();   
+        verificarValor();
     });
 });
 
 document.querySelector(".apagar").addEventListener("click", function(){
-    let valor = document.querySelector("#conta").value;
-    let res = valor.slice(0, -1);
-    document.querySelector("#conta").value = res;
-    if(res === ""){
-        document.querySelector(".apagar").style.display = 'none';
-    }
+    apagar();
 });
+
 
 document.querySelector(".power").addEventListener("click", function(){
     if(this.style.backgroundColor === "green"){
         document.querySelector(".display").style.borderColor = "green";
     }else{
         document.querySelector(".display").style.borderColor = "red";
+    }
+});
+
+document.addEventListener("keyup", function(a){
+    if(document.querySelector("#conta").value != "DESLIGADO"){
+        if(a.key >= 0 && a.key <=9){
+            if(document.querySelector("#conta").value === "LIGADO"){
+                document.querySelector("#conta").value = a.key;
+            }else{
+                document.querySelector("#conta").value += a.key;
+            }
+        }
+        switch(a.key){
+            case "Backspace":
+                if(document.querySelector("#conta").value != "LIGADO" && document.querySelector("#conta").value != "DESLIGADO"){
+                    apagar();
+                }
+            break;
+            case "/":
+                if(ultimoCaracter() >= 0 && ultimoCaracter() <=9 && ultimoCaracter() != ""){
+                    document.querySelector("#conta").value += a.key;
+                }
+            break;
+            case "*":
+                if(ultimoCaracter() >= 0 && ultimoCaracter() <=9 && ultimoCaracter() != ""){
+                    document.querySelector("#conta").value += a.key;
+                }
+            break;
+            case "-":
+                if(ultimoCaracter() >= 0 && ultimoCaracter() <=9 && ultimoCaracter() != ""){
+                    document.querySelector("#conta").value += a.key;
+                }
+            break;
+            case "+":
+                if(ultimoCaracter() >= 0 && ultimoCaracter() <=9 && ultimoCaracter() != ""){
+                    document.querySelector("#conta").value += a.key;
+                }
+            break;
+            case ",":
+                if(ultimoCaracter() >= 0 && ultimoCaracter() <=9 && ultimoCaracter() != ""){
+                    document.querySelector("#conta").value += a.key;
+                }
+            break;
+            case "%":
+                if(ultimoCaracter() >= 0 && ultimoCaracter() <=9 && ultimoCaracter() != ""){
+                    document.querySelector("#conta").value += a.key;
+                }
+            break;
+            case "=":
+                alert('deu enter!');
+            break;
+        }
+        verificarValor();
+    }else{
+        alert("calculadora desligada!"); 
     }
 });
